606137693bb520f29e2c28cfa073f24f224e9d62
wjdaude <jmlee7733@gmail.com>
2026-03-09 07:58:43 +0900

    feat: add selection sort

---
+++ selection sort.c
@@ -0,0 +1,14 @@
+void selection_sort(int list[],int n)
+{
+    int i,j,min,t;
+    for(i=0;i<n;i++){
+        min=i;
+        for(j=i+1;j<n;j++){
+            if(list[j]<list[min])
+            min=j;
+        }
+            t=list[i];
+            list[i]=list[min];
+            list[min]=t;
+        }
+    }

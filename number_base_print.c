ce5ad65dd5feaeae1be6798603d04bb018b05e1d
wjdaude <jmlee7733@gmail.com>
2026-03-09 19:54:42 +0900

    add: variable base format example

---
+++ number_base_print.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a;
+    a=127;
+    printf("a의 값은 %d진수로 %o입니다.\n",8,a);
+     printf("a의 값은 %d진수로 %d입니다.\n",10,a);
+      printf("a의 값은 %d진수로 %x입니다.\n",16,a);
+}
